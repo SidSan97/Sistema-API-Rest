141c7e26fd956280e7e8049a16d0ee6fbe3ff593
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-02-19 23:38:15 -0300

    criação e adição de arquivos

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class EventsController extends Controller
+{
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function pagina_cadastro()
+    {
+        return view('cadastrar-cliente');
+    }
+
+    public function form_cadastro(Request $request)
+    {
+        $event = new Event;
+
+        $event->nome  = $request->nome;
+        $event->tel   = $request->tel;
+        $event->cpf   = $request->cpf;
+        $event->placa = $request->placa;
+
+        $event->save();
+
+        return redirect('/');
+    }
+
+    public function edit_cadastro()
+    {
+        
+    }
+}
---
+++ resources/views/cadastrar-cliente.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+    </script>
+
+    <title>Cadastrar</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <form action="/enviar" method="post">
+            @csrf
+            <h1>Cadastro do(a) cliente</h1> 
+                <div class="row mb-3">
+                    <div class="col-md-6 mb-2">
+                        <label for="nome">Nome:</label>
+                        <input type="text" name="nome" id="nome" class="form-group" required maxlenght="200">
+                    </div>
+
+                    <div class="col-md-6 mb-2">
+                        <label for="tel">Telefone:</label>
+                        <input type="text" name="tel" id="tel" class="form-group" required maxlenght="15">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="col-md-6 mb-2">
+                        <label for="cpf">CPF:</label>
+                        <input type="text" name="cpf" id="cpf" class="form-group" required maxlenght="14">
+                    </div>
+
+                    <div class="col-md-6 mb-2">
+                        <label for="placa">Placa do veículo:</label>
+                        <input type="text" name="placa" id="placa" class="form-group" required maxlenght="8">
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">enviar</button>
+            </form>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+    <script src="../js/validador-cpf.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+</body>
+</html>
